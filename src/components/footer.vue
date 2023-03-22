53a8afe79b4f0c8e7638e2cab0e14213dbe86e16
Hosea <hoseacorpuz@gmail.com>
2023-03-22 13:21:19 +0800

    fix server error

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <footer>
+    <section>
         <div class="text-center p-6 text-slate-500">
             <span>Developed by Hosea Patricia Corpuz | ©2023 Copyright</span>
         </div>
-    </footer>
+    </section>
 </template>
